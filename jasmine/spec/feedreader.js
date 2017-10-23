8221de1382e1f74f2f0252de2cebcba04273b022
Matt Bledsoe <mattbledsoe@Matts-MacBook-Pro.local>
2017-10-23 18:05:27 -0400

    finish project

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -59,64 +59,76 @@ $(function() {
 
     describe('The Menu', function() {
 
-    /* TODO: Write a test that ensures the menu element is
-     * hidden by default. You'll have to analyze the HTML and
-     * the CSS to determine how we're performing the
-     * hiding/showing of the menu element.
-     */
-     it('is hidden by default', function() {
-      //  expect(document.querySelector('menu-hidden')).toBe(true); why wont this work?
-       expect($('body').hasClass('menu-hidden')).toBe(true);
-     });
-    /* TODO: Write a test that ensures the menu changes
-     * visibility when the menu icon is clicked. This test
-     * should have two expectations: does the menu display when
-     * clicked and does it hide when clicked again.
-     */
-     it('changes visibility when clicked', function() {
-       var slideMenu = $('body').hasClass('slide-menu');
-       if (slideMenu.display === 'block') {
-         expect(slideMenu).toBe(true);
-       } else {
+      /* TODO: Write a test that ensures the menu element is
+       * hidden by default. You'll have to analyze the HTML and
+       * the CSS to determine how we're performing the
+       * hiding/showing of the menu element.
+       */
+      it('is hidden by default', function() {
+        expect($('body').hasClass('menu-hidden')).toBe(true);
+      });
+      /* TODO: Write a test that ensures the menu changes
+       * visibility when the menu icon is clicked. This test
+       * should have two expectations: does the menu display when
+       * clicked and does it hide when clicked again.
+       */
+      it('changes visibility when clicked', function() {
+        var slideMenu = $('body').hasClass('slide-menu');
+        if (slideMenu.display === 'block') {
+          expect(slideMenu).toBe(true);
+        } else {
           expect(slideMenu).not.toBe(true);
-       }
+        }
 
-     });
+      });
 
-  });
+    });
     /* TODO: Write a new test suite named "Initial Entries" */
 
     describe('Initial Entries', function() {
 
-    /* TODO: Write a test that ensures when the loadFeed
-     * function is called and completes its work, there is at least
-     * a single .entry element within the .feed container.
-     * Remember, loadFeed() is asynchronous so this test will require
-     * the use of Jasmine's beforeEach and asynchronous done() function.
-     */
-     beforeEach(function(done) {
-       loadFeed(function() {
-         done();
-       });
-     });
+      /* TODO: Write a test that ensures when the loadFeed
+       * function is called and completes its work, there is at least
+       * a single .entry element within the .feed container.
+       * Remember, loadFeed() is asynchronous so this test will require
+       * the use of Jasmine's beforeEach and asynchronous done() function.
+       */
+      beforeEach(function() {
+        loadFeed(0, function() {  // sims async loadFeed
+          done();
+        });
+      });
 
-     it('displays an entry after loadFeed is called', function() {
-       var content = $('body').hasClass('entry');
-       expect(content.display === 'block').toBe(true);
-     });
-  });
+      it('displays after loadFeed is called', function() {
+        expect('.entry').toBeDefined(true);   // checks for loadFeed to work
+      });
+    });
 
 
     /* TODO: Write a new test suite named "New Feed Selection" */
-    describe('New Feed Selection', function(){
+    describe('New Feed Selection', function() {
 
-    /* TODO: Write a test that ensures when a new feed is loaded
-     * by the loadFeed function that the content actually changes.
-     * Remember, loadFeed() is asynchronous.
-     */
-     it('content changes when loadFeed is called', function() {
+      var priorState;
 
-     });
+      beforeEach(function() {
+        loadFeed(0, function() {
+          done();
+          priorState = $('.entry').html(); // saves previous loadFeed
+
+          loadFeed(1, function() {  // reloads loadFeed
+            done();
+          });
+        });
+      });
+
+      /* TODO: Write a test that ensures when a new feed is loaded
+       * by the loadFeed function that the content actually changes.
+       * Remember, loadFeed() is asynchronous.
+       */
+
+      it('content changes when loadFeed is called', function() {
+        expect('.entry').html()).not.toBe(priorState);  // determines whether loadFeed correctly loaded new content
+      });
 
     });
 
